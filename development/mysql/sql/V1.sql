efb87134daa13956911cb9cd68d602b32188a6f9
河田鷹介 <benefium@gmail.com>
2022-04-16 23:14:35 +0900

    Revert "mysql設定変更"
    This reverts commit 960c99311a7f3ae17ba8d002b93939ba6d762331.

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -5,4 +5,3 @@ alter table record add index r_list_order2(updated_at desc, record_id asc);
 alter table record add index r_tome(category_id, application_group);
 alter table record_item_file add index rif_linked_record_id(linked_record_id);
 alter table record_comment add index rc_linked_record_id(linked_record_id);
-alter table record_comment add index rc_created_by(created_by);
