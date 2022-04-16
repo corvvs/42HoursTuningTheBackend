ef5da3548779b2dd7b8feca872aad282bddcf8ae
河田鷹介 <benefium@gmail.com>
2022-04-16 13:55:23 +0900

    record: update_at desc, record_id asc インデックスを作る

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,5 +1,6 @@
 alter table session add index session_value(value);
 alter table record add index r_created_by(created_by);
 alter table record add index r_status(status);
+alter table record add index r_list_order2(updated_at desc, record_id asc);
 alter table record_item_file add index rif_linked_record_id(linked_record_id);
 alter table record_comment add index rc_linked_record_id(linked_record_id);
